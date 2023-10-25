2d295eed0690da70bf9bcb3b91237cf753ab4aea
Lakshita Ganesh Kumar <lakshita.ganesh_kumar@uconn.edu>
2023-10-25 10:43:22 -0400

    main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+# Display welcome message
+
+# Get user input for contestant objects
+# Contestant Object attributes: 
+    # name - name of contestant
+    # perfect match - contestant's perfect match
+    # found perfect match - True/False if contestant found their perfect match
+    # invalid matches - list of every but the contestant and their perfect match
+    # current partner - current partner during this week
+    # current partner is valid - True or False if current partnet is perfect match or not
+    # known invalid matches - list of matches that are known to be invalid in the gameplay
+    # possible matches - list of possible matches based on who we know to be an invalid match. (Everyone but the contestant and their invalid matches)
+# Contestant object methods: getter and setter methods
+
+# Randomly choose 8 perfect pairs
+# Update person objects w/ their pairs
+
+# Track number of weeks played
+
+# Randomly pair up contestants
+
+# Check if all matches are found
+
+# Display match results (2 matches found, no matches found, all matches found)
+
+# Get user input for truth booth
+    # Is the match at the truth booth a valid match
+    # Update contestant objects accordingly
+
+# Pair contestants to people they haven't been paired before
+    # Randomly choose from possible matches attribute
+
+# Repeat process of checking if all matches are found, displaying results, and going to the truth booth
