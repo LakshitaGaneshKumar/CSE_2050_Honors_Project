f3cb562167e6f5fb5bbacb560ea0c24afb66b68b
Lakshita Ganesh Kumar <lakshita.ganesh_kumar@uconn.edu>
2023-10-25 10:57:24 -0400

    Contestant Class

---
+++ Contestant.py
@@ -0,0 +1,38 @@
+class Contestant():
+    def __init__(self, name):
+        """Initialize a Contestant Object"""
+        self._name = name
+        self._perfect_match = None
+        self._found_perfect_match = False
+        self._invalid_matches = None
+        self._current_partner = None
+        self._known_invalid_match = None
+        self._possible_matches = None
+
+    def get_name(self): pass
+
+    def get_perfect_match(self): pass
+
+    def get_found_perfect_match(self): pass
+
+    def get_invalid_matches(self): pass
+
+    def get_current_partner(self): pass
+
+    def get_known_invalid_matches(self): pass
+
+    def get_possible_matches(self): pass
+
+    def set_name(self, name): pass
+
+    def set_perfect_match(self, perfect_match): pass
+
+    def set_found_match(self, found_match): pass
+
+    def set_invalid_matches(self, invalid_matches): pass
+
+    def set_current_partner(self, current_partner): pass
+
+    def set_known_invalid_matches(self, invalid_matches): pass
+
+    def set_possible_matches(self, possible_matches): pass
